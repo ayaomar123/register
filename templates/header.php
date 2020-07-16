67c6a5ff2cb3a6293b820044417e228cc2dbf7c0
ayaomar123 <44114766+ayaomar123@users.noreply.github.com>
2020-07-16 13:31:43 +0300

    Add files via upload

---
+++ templates/header.php
@@ -0,0 +1,29 @@
+<head>
+  <title>MY LOG IN</title>
+  <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style type="text/css">
+    .brand{
+      background: pink  !important;
+    }
+    .brand-text{
+      color:pink  !important;
+    }
+    .input-text{
+        color: pink  !important;
+    }
+    form{
+      max-width: 460px;
+      margin: 20px auto;
+      padding: 20px;
+    }
+  
+    </style>
+</head>
+<body class="white lighten-4">
+  <nav class=" z-depth-0 pink">
+    <div class="container">
+      <a href="index.php" class="brand-logo center">LOG IN</a>
+    </div>
+  </nav>
+ 
